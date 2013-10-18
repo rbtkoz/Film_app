aa0c9b21a83ac5743c8f9c01650877c3131946de
AK <kozovski.a@gmail.com>
2013-10-18 13:43:57 -0400

    first commit Film app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FilmsApp::Application.config.secret_key_base = '2a2098251d5387313a4754e129423f8890a03105ff42de1c2a49b91fbe1bf5e55846bc290a6e7ab264c832189c997daf6569062bd4cbbe3f8dee7c2e0dff8b4a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FilmsApp::Application.config.session_store :cookie_store, key: '_films_app_session'
